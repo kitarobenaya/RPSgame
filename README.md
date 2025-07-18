7506096931e2c3febeb40fe0ea9fcd59c3e0b2a6
kitarobenaya <kitarobenaya09@gmail.com>
2025-07-18 08:34:37 +0700

    menambahkan animasi imageRoll dalam script.js dan mengupdate isi README.md

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # gameSuit
+
+Game suit simple, dengan computer sebagai lawannya.
+
+## 🧰 Teknologi yang Digunakan
+- HTML5.
+- CSS3.
+- JavaScript (Vanilla).
+
+## 🌐 Live Demo
+[Klik untuk mencoba aplikasi](https://kitarobenaya.github.io/gameSuit/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@ const player_Images = document.querySelectorAll(".player-image");
 const imgComp = document.querySelector(".computer-image");
 const compCapt = document.querySelector(".computer-caption-image");
 const info = document.querySelector("section.info");
-const scorePlayer = document.querySelector('div.score.player');
-const scoreComp = document.querySelector('div.score.computer');
+const scorePlayer = document.querySelector("div.score.player");
+const scoreComp = document.querySelector("div.score.computer");
 
 function getComputerChoice() {
   const choice = ["gajah", "orang", "semut"];
@@ -21,18 +21,33 @@ function getTheWinner(playerChoice, computerChoice) {
     return computerChoice == "semut" ? "Menang!" : "Kalah!";
 }
 
+function imageRoll() {
+  const gambar = ["gajah", "orang", "semut"];
+  let i = 0;
+  const interval = setInterval(() => {
+    imgComp.setAttribute("src", `assets/images/${gambar[i++]}.png`);
+    if(i == gambar.length) return i = 0;
+    setTimeout(() => {
+      clearInterval(interval);
+    }, 900)
+  }, 100);
+}
+
 player_Images.forEach((img) => {
   img.addEventListener("click", function () {
     const playerChoice = this.dataset.choice;
+    imageRoll();
     const computerChoice = getComputerChoice();
-    console.log(computerChoice)
+    console.log(computerChoice);
+    setTimeout(() => {
       imgComp.setAttribute("src", `assets/images/${computerChoice}.png`);
       compCapt.innerHTML = computerChoice;
       info.innerHTML = getTheWinner(playerChoice, computerChoice);
-      if(info.innerHTML == "Menang!") {
+      if (info.innerHTML == "Menang!") {
         scorePlayer.innerHTML = parseInt(scorePlayer.innerHTML) + 1;
-      }else if(info.innerHTML == "Kalah!"){
+      } else if (info.innerHTML == "Kalah!") {
         scoreComp.innerHTML = parseInt(scoreComp.innerHTML) + 1;
       }
+    }, 1000)
   });
 });
